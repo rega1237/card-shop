58ab1868362f3e13947e971da6338b1fcb09a641
rega1237 <rega1237@gmail.com>
2022-11-13 10:55:29 -0400

    linters errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,7 @@ class Product < ApplicationRecord
   has_many :category, through: :product_categories
   has_one_attached :image
 
-  private
-
   def self.latest_products
     Product.order('created_at DESC').limit(8)
   end
-  
 end
